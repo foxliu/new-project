685bd233dbed933063158cff2821486578d9213a
foxliu2012 <foxliu2012@gmail.com>
2013-09-25 16:28:02 +0800

    update

--- changemysql.sh
+++ changemysql.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-#Database=$(ls /usr/local/mysql/data/ | grep \/ | grep -Ev '(bak|performance)')
+Logfile=/root/tools/change.log
 
-for i in mysql cfp_swap crm2 crm4 test
+Backupdata () {
+  for i in mysql cfp_swap crm2 crm4 test
+  do
+    mysqldump -S /tmp/mysqld.sock --opt --database $i > /data/sql/$i && mysql -S /tmp/mysql.sock < /data/sql/$i
+  done
+}
+
+Stopmysql () {
+  netstat -tlunp | grep '3309' | awk '{print $7}' | sed 's/\/.*$//g' | xargs kill
+  /etc/init.d/mysql stop
+}
+
+Changefile () {
+  rm -f /usr/local/mysql && ln -s /usr/local/mysql-5.1.30-linux-x86_64-glibc23 /usr/local/mysql
+}
+
+echo -e "==============Change Start===============" >> $Logfile 
+Backupdata >> $Logfile
+Stopmysql >> $Logfile
+while [ "$(netstat -tlunp | grep -E '(3306|3309)')" != "" ]
 do
-  mysqldump -S /tmp/mysqld.sock --opt --database $i > /data/sql/$i && mysql -S /tmp/mysql.sock < /data/sql/$i
+  Stopmysql >> $Logfile
 done
-
+Changefile  >> $Logfile
+/etc/init.d/mysql start >>$Logfile 
+echo -e "\n==================Change Complete============="
