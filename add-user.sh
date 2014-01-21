f98073c18e00987498781e7aaf407781048bb383
taylor <taylor@visualchina.(none)>
2014-01-21 17:09:34 +0800

    update

---
+++ add-user.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USER[0]=
+
+USERADD='/usr/sbin/useradd'
+PASSWD='/usr/bin/passwd'
+
+for users in ${USER[@]}
+do
+  user=$(echo $users | awk -F";" '{print $1}')
+  pass=$(echo $users | awk -F";" '{print $2}')
+  
+  $USERADD -g wheel $user && echo $pass | $PASSWD --stdin $user
+  sleep 1
+done
+
+exit 0
