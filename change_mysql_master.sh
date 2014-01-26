085f5e6a1e011ebd66c2ed96e14cb27abd75acd4
liusonghui <songhui.liu@visualchina.com>
2014-01-26 18:00:19 +0800

    update

--- change_mysql_master.sh
+++ change_mysql_master.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-DBuser=root
-DBpass=zaq12wsxz
-DBip="219.239.94.108 27.112.3.76"
-VIP=219.239.94.117
-MYSQL='/usr/local/mysql/bin/mysql'
-PING='/bin/ping'
-IFCONFIG='/sbin/ifconfig'
-Slave_master=219.239.94.106
+export LANG=en.US_UTF-8
 
+# Setting
+DBuser=
+DBpass=
+Slaves=""
+VIP=
+Slave_master=
+
+# Get the path of the progress which is needed
+MYSQL="$(which mysql) -u$DBuser -p$DBpass"
+PING="$(which ping)"
+IFCONFIG="$(which ifconfig)"
+IP="$(which ip)"
+
+# Check the local service is the mysql master or not
 $IFCONFIG | grep $VIP >/dev/null 2>&1
 if [ "$?" == 0 ]; then
     echo "The Mysql Master Service is Localhost ! Don't execute $0 on this service"
     exit 0
 fi
-
-# Get Master info
-Master_Info=$($MYSQL -u$DBuser -p$DBpass -e "show master status\G" | grep -E '(File|Position)')
-log_file=$(echo $Master_Info | grep File | awk '{print $2}')
-log_pos=$(echo $Master_Info | grep Position | awk '{print $4}')
 
 # Check the VIP is or not alive
 $PING $VIP -c 5 -w 5 >/dev/null 2>&1  && if [ "$?" == 0 ]; then
@@ -27,7 +29,11 @@ fi
 
 # Wait the Slave's synchronization is complete
 Get_slave_behind_master_second () {
-  $MYSQL -u$DBuser -p$DBpass -e "show slave status\G" | grep Seconds_Behind_Master | awk '{print $2}'
+    if [ -z $1 ]; then
+        $MYSQL -e "show slave status\G" | grep Seconds_Behind_Master | awk '{print $2}'
+    else
+        $MYSQL -h $1 -e "show slave status\G" | grep Seconds_Behind_Master | awk '{print $2}'
+    fi
 }
 if [ "$(Get_slave_behind_master_second)" = "NULL" ]; then
   echo "This Mysql service is not synchrond from Mysql master. Try other service , or try use $MYSQL -u <user> -p and enter start slave; Try again"
@@ -40,4 +46,27 @@ do
 done
 
 # Change Local service to Mysql master
+$MYSQL -e "stop slave;"
+# Get Master info
+Master_Info=$($MYSQL -e "show master status\G" | grep -E '(File|Position)')
+log_file=$(echo $Master_Info | grep File | awk '{print $2}')
+log_pos=$(echo $Master_Info | grep Position | awk '{print $4}')
+# Add VIP on local service
+$IP addr add ${VIP}/24 broadcast + dev eth0 label eth0:1
 
+# Change the master info on slave service
+for slave in $Slaves
+do
+    # check the slave is or not behind the master
+    while [ "$(Get_slave_behind_master_second $slave)" != "0" ]
+    do
+        echo "Wait the Slave $slave is synchronding...."
+        sleep 1
+    done
+    # stop slave and change master info restart the slave
+    $MYSQL -h $slave -e "stop slave;"
+    $MYSQL -h $slave -e "change master to master_host=${VIP}, master_user='bak43ph', master_password='comphoto', master_log_file=${log_file}, master_log_pos=${log_pos};"
+    $MYSQL -h $slave -e "start salve;"
+done
+
+exit 0
---
+++ defense_down.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+export LANG=en_US.UTF-8
+FILE=''
+THRESHOLD=100
+Load_thre=10
+
+# check the load is warn or not, if the load < threshold , exit the progress
+load_average=$(uptime | awk '{print $(NF-2)}' | sed -e 's/,//g')
+load_check=$(awk -v now=$load_average -v thre=$Load_thre 'BEGIN{print(now >= thre)?"1":"0"}')
+if [ "$load_check" -eq 0 ]; then
+    exit 0
+fi
+
+# get the file and path
+DATE=$(date "+%Y%m%d%H")
+Y=$(expr substr $DATE 1 4)
+M=$(expr substr $DATE 5 2)
+D=$(expr substr $DATE 7 2)
+H=$(expr substr $DATE 9 2)
+File=/var/log/apache2/$Y/$M/$D/${FILE}_${H}-access_log
+
+# get Less than 10 minutes which ip's accesses is more than Threshold 
+date_10min_ago=$(date --date="10 minutes ago" "+%Y:%H:%M:%S")
+date_now=$(date "+%Y:%H:%M:%S")
+for ip in $(cat $File | sed -n "/$date_10min_ago/,/$date_now/ p"| awk -v count=$THRESHOLD '{a[$1]++}END{for (i in a) if (a[i] > count) print i}')
+do
+    # deny ip to access and check the ip was denied or not
+    ufw status | grep $ip | grep DENY
+    if [ $? -ne 0 ]; then
+	echo "$date_now  $ip > $THRESHOLD defense" >> /var/log/defense_down.log
+	ufw insert 1 deny from $ip to any
+    fi
+done
+
+exit 0
