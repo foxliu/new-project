0504f1e760d2a66b3595138067c052afe9ee862b
liusonghui <songhui.liu@visualchina.com>
2014-04-02 09:48:46 +0800

    idont know why

--- bank_rsync.sh
+++ bank_rsync.sh
@@ -18,8 +18,25 @@ DIFF='/usr/bin/diff'
 RSYNC='/usr/bin/rsync'
 MKFIFO='/usr/bin/mkfifo'
 
-$LS $PATH_UPLOAD > $LOGFILE
-$LS $SRC_UPLOAD > $NEW_LOG
+function trap_exit
+{
+	kill -9 0
+}
+
+exec_commod() { 
+	rsync -av 219.239.94.111:$1/$2 $1/
+	if [ $? -ne 0 ]; then
+        echo "command execute failure"
+        return 1
+    fi
+}
+
+########################Script start#########################
+FAILURE_FLAG=/var/log/failure.log
+trap 'trap_exit; exit2' 1 2 3 15
+
+$LS -t $PATH_UPLOAD > $LOGFILE
+$LS -t $SRC_UPLOAD > $NEW_LOG
 
 TEMP_FILE=/tmp/.rsync_file
 TEMP_FIFO=/tmp/.$$_fifo
@@ -32,9 +49,30 @@ $MKFIFO $TEMP_FIFO
 exec 6<>$TEMP_FIFO
 rm -f $TEMP_FIFO
 {
-	for (i=1;i<=4;i++)
+	for (i=1;i<=2;i++)
 	do
 		echo
 	done
 }>&6
 
+Src_img=$(echo $SRC_IMG | sed -e 's/\/$//')
+Src_upload=$(echo $SRC_UPLOAD | sed -e 's/\/$//')
+
+while read SEC
+do
+	read <&6
+	( exec_commod $Src_img $SEC || echo $SEC >> $FAILURE_FLAG )&
+	( exec_commod $Src_upload $SEC || echo $SEC >> $FAILURE_FLAG ; echo >&6 )&
+done <$TEMP_FILE
+
+wait
+
+exec 6>&-
+
+rm -f $TEMP_FILE
+
+if [ -f $FAILURE_FLAG ];then
+	exit 1
+else
+	exit 0
+fi
